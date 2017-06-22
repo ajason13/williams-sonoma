6bf983826bb7bf66454cfbcc1545c8d03364058e
ajason13 <a.jason13@gmail.com>
2017-06-21 20:25:19 -0700

    Added request methods to AppBasics.java

--- src/main/java/com/sqa/ja/helpers/AppBasics.java
+++ src/main/java/com/sqa/ja/helpers/AppBasics.java
@@ -11,6 +11,7 @@ public class AppBasics {
 	 */
 	public static void farewellUser(String name) {
 		System.out.print("Thanks for using this app.\nHave a great day " + name + "!");
+		scan.close();
 	}
 
 	/**
@@ -22,5 +23,39 @@ public class AppBasics {
 		System.out.println("Welcome to the " + appName + " Application");
 		System.out.print("Could I get your name please: ");
 		return scan.nextLine();
+	}
+
+	public static boolean requestBoolean(String question) {
+		System.out.println(question);
+		return scan.nextBoolean();
+	}
+
+	public static char requestChar(String question) {
+		System.out.println(question);
+		char c = scan.next().charAt(0);
+		return c;
+	}
+
+	public static float requestFloat(String question) {
+		System.out.println(question);
+		return scan.nextFloat();
+	}
+
+	public static long requestLong(String question) {
+		System.out.println(question);
+		return scan.nextLong();
+	}
+
+	public static short requestShort(String question) {
+		System.out.println(question);
+		return scan.nextShort();
+	}
+
+	public static String requestString(String question) {
+		System.out.println(question);
+		scan.nextLine();
+		String s = scan.nextLine();
+		scan.nextLine();
+		return s;
 	}
 }
