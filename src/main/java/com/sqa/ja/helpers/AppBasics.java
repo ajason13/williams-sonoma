9555cdcc0f6aa691d1b3c20475f3efc8851713ac
ajason13 <a.jason13@gmail.com>
2017-06-22 18:27:32 -0700

    Added request methods for AppBasics.java and added exception classes for catching exceptions

--- src/main/java/com/sqa/ja/helpers/AppBasics.java
+++ src/main/java/com/sqa/ja/helpers/AppBasics.java
@@ -2,6 +2,8 @@ package com.sqa.ja.helpers;
 
 import java.util.*;
 
+import com.sqa.ja.helpers.exceptions.*;
+
 public class AppBasics {
 
 	private static Scanner scan = new Scanner(System.in);
@@ -25,37 +27,204 @@ public class AppBasics {
 		return scan.nextLine();
 	}
 
+	/**
+	 * Method to request a Boolean response from user
+	 *
+	 * @param question
+	 *            Question to ask the user
+	 * @return Boolean response from user
+	 */
 	public static boolean requestBoolean(String question) {
-		System.out.println(question);
-		return scan.nextBoolean();
+		boolean isInvalid = true;
+		String input = "";
+		while (isInvalid) {
+			System.out.print(question + " (true/false) ");
+			input = scan.nextLine();
+			try {
+				if (!(input.equalsIgnoreCase("true") || input.equalsIgnoreCase("false"))) {
+					throw new InvalidBooleanResponse();
+				}
+				isInvalid = false;
+			} catch (InvalidBooleanResponse e) {
+				System.out.println("You didn't answer true or false [" + input + "]");
+			}
+		}
+		return Boolean.parseBoolean(input.trim());
 	}
 
+	/**
+	 * Method to request a Byte response from user
+	 *
+	 * @param question
+	 *            Question to ask the user
+	 * @return Byte response from user
+	 */
+	public static byte requestByte(String question) {
+		byte value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scan.nextLine();
+			try {
+				value = Byte.parseByte(input.trim());
+				isInvalid = false;
+			} catch (Exception e) {
+				System.out.println("You have not entered a correct formatted number [" + input + "]");
+			}
+		}
+		return value;
+	}
+
+	/**
+	 * Method to request a Char response from user
+	 *
+	 * @param question
+	 *            Question to ask the user
+	 * @return Char response from user
+	 */
 	public static char requestChar(String question) {
-		System.out.println(question);
-		char c = scan.next().charAt(0);
-		return c;
+		boolean isInvalid = true;
+		String input = "";
+		while (isInvalid) {
+			System.out.print(question + " ");
+			input = scan.nextLine();
+			try {
+				if (input.length() > 1) {
+					throw new InvalidCharRepsonseLength();
+				}
+				isInvalid = false;
+			} catch (InvalidCharRepsonseLength e) {
+				System.out.println("You have entered too many characters [" + input + "]");
+			}
+		}
+		return input.charAt(0);
 	}
 
+	/**
+	 * Method to request a Double response from user
+	 *
+	 * @param question
+	 *            Question to ask the user
+	 * @return Double response from user
+	 */
+	public static double requestDouble(String question) {
+		double value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scan.nextLine();
+			try {
+				value = Double.parseDouble(input.trim());
+				isInvalid = false;
+			} catch (Exception e) {
+				System.out.println("You have not entered a correct formatted number [" + input + "]");
+			}
+		}
+		return value;
+	}
+
+	/**
+	 * Method to request a Float response from user
+	 *
+	 * @param question
+	 *            Question to ask the user
+	 * @return Boolean response from user
+	 */
 	public static float requestFloat(String question) {
-		System.out.println(question);
-		return scan.nextFloat();
+		float value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scan.nextLine();
+			try {
+				value = Float.parseFloat(input.trim());
+				isInvalid = false;
+			} catch (Exception e) {
+				System.out.println("You have not entered a correct formatted decimal number [" + input + "]");
+			}
+		}
+		return value;
 	}
 
+	/**
+	 * Method to request a Int response from user
+	 *
+	 * @param question
+	 *            Question to ask the user
+	 * @return Int response from user
+	 */
+	public static int requestInt(String question) {
+		int value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scan.nextLine();
+			try {
+				value = Integer.parseInt(input.trim());
+				isInvalid = false;
+			} catch (Exception e) {
+				System.out.println("You have not entered a correct formatted number [" + input + "]");
+			}
+		}
+		return value;
+	}
+
+	/**
+	 * Method to request a Long response from user
+	 *
+	 * @param question
+	 *            Question to ask the user
+	 * @return Long response from user
+	 */
 	public static long requestLong(String question) {
-		System.out.println(question);
-		return scan.nextLong();
+		long value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scan.nextLine();
+			try {
+				value = Long.parseLong(input.trim());
+				isInvalid = false;
+			} catch (Exception e) {
+				System.out.println("You have not entered a correct formatted number [" + input + "]");
+			}
+		}
+		return value;
 	}
 
+	/**
+	 * Method to request a Short response from user
+	 *
+	 * @param question
+	 *            Question to ask the user
+	 * @return Short response from user
+	 */
 	public static short requestShort(String question) {
-		System.out.println(question);
-		return scan.nextShort();
+		short value = 0;
+		boolean isInvalid = true;
+		while (isInvalid) {
+			System.out.print(question + " ");
+			String input = scan.nextLine();
+			try {
+				value = Short.parseShort(input.trim());
+				isInvalid = false;
+			} catch (Exception e) {
+				System.out.println("You have not entered a correct formatted number [" + input + "]");
+			}
+		}
+		return value;
 	}
 
+	/**
+	 * Method to request a String response from user
+	 *
+	 * @param question
+	 *            Question to ask the user
+	 * @return String response from user
+	 */
 	public static String requestString(String question) {
-		System.out.println(question);
-		scan.nextLine();
+		System.out.print(question + " ");
 		String s = scan.nextLine();
-		scan.nextLine();
-		return s;
+		return s.trim();
 	}
 }
---
+++ src/main/java/com/sqa/ja/helpers/exceptions/InvalidBooleanResponse.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: InvalidBooleanResponse.java<br>
+ *
+ *   Alvarez, Jason<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Jun 21, 2017
+ *   
+ */
+
+package com.sqa.ja.helpers.exceptions;
+
+
+/**
+ * InvalidBooleanResponse //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Alvarez, Jason
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class InvalidBooleanResponse extends Exception {
+}
